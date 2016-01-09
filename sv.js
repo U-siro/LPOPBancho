076e242199c5bc3a8526d113f0e83bd99b9b25a2
KangYui <unconfigured@null.spigotmc.org>
2016-01-09 12:44:10 +0900

    Really Source!

---
+++ sv.js
@@ -0,0 +1,143 @@
+console.log("Defining variable and modules");
+var express = require('express');
+var app = express();
+var users=new Array();
+var fs = require('fs');
+var mysql = require('mysql');
+var conv = require('binstring');
+var packets=[];
+var failed;
+
+console.log("Reading Packets");
+// Packets
+fs.readFile('C:\\Users\\lpopvm\\Desktop\\Bancho\\login.raw', 'utf8', function (err,data) {
+packets.push(conv(data,{in:'binary', out:'buffer'}));
+
+console.log("Login Packet Successfully Saved!");
+});
+
+
+console.log("Enabling PHP");
+var phpnode = require('php-node')({bin:"C:\\xampp\\php\\php.exe"});
+app.engine('php', phpnode);
+app.set('view engine', 'php');
+
+console.log("Defining Functions");
+function rawBody(req, res, next) {
+  req.setEncoding('utf8');
+  req.rawBody = '';
+  req.on('data', function(chunk) {
+    req.rawBody += chunk;
+  });
+  req.on('end', function(){
+    next();
+  });
+}
+
+
+function endsWith(str, suffix) {
+    return str.indexOf(suffix, str.length - suffix.length) !== -1;
+}
+
+function makeid()
+{
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for( var i=0; i < 5; i++ )
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    return text;
+}
+
+function replaceAll(str, searchStr, replaceStr) {
+
+    return str.split(searchStr).join(replaceStr);
+}
+
+console.log("Express.js Preparing..");
+  app.use(rawBody);
+app.set('views', "C:\\Users\\lpopme\\Downloads\\Server\\Server\\");
+console.log("MySQL Connecting..");
+  var connection = mysql.createConnection({
+    host    :'ranking.lpop.me',
+    port : 3306,
+    user : 'root',
+    password : '***REMOVED***',
+    database:'osuserve_osuserver'
+});
+connection.connect(function(err) {
+    if (err) {
+        console.error('mysql connection error');
+        console.error(err);
+        throw err;
+    } else {
+      console.log("MySQL Successfully Connected");
+    }
+});
+////var compress = require('compression');
+
+//app.use(compress());  
+console.log(packets['failed']);
+app.post('/', function(req, res) {
+console.log("Bancho Request");
+console.log(req.rawBody);
+var reqcon=req.rawBody;
+reqcon=reqcon.split(/\n/);
+
+  if(req.get("osu-token")){
+
+    res.send("");
+    return;
+  }
+if(reqcon[0]=="lockme"){
+  res.sendFile('C:\\Users\\lpopvm\\Desktop\\Bancho\\lock.raw');
+  return;
+}
+//res.set("Content-Encoding","gzip");
+
+res.set("cho-token","yomama");
+res.set("cho-protocol","19");
+        var sql="SELECT * FROM users_accounts where osuname='" + reqcon[0] + "' and passwordHash='" + reqcon[1] + "';"
+    connection.query(sql,function(err,rows){
+
+  if(!rows[0]){ 
+
+res.sendFile('C:\\Users\\lpopvm\\Desktop\\Bancho\\failed.raw');
+console.log("Login Failed Packet Successfully Saved!");
+    console.log("Login Failed");
+    return;
+  } else {
+  var out=conv(packets[0],{in:'buffer', out:'binary'});
+console.log(reqcon[0]);
+  out=replaceAll(out,"LPOPYui",reqcon[0]);
+  res.send(out);
+  console.log(out);
+  }
+});
+
+
+    //res.send(".................`...K..........G..........H..\n.............S......`.....LPOPYui......................`.............................................S..!.........CassandraBot...............S............Connor...........-...S......}.....Doomsday...........4...`..\n.........`...Y..........@........#osu@........#newsA........#osu..Main channel..A..F.....#news.;This will contain announcements and info, while beta lasts......:.....CassandraBot. Holy shit dude, it's working! :D..#osu.......'.....CassandraBot.\nThanks JustM3..#osu.......=.....CassandraBot.#Checkout #news for more information..#osu.......Z.....CassandraBot.?Bancho implementation is a go! Thanks to JustM3 for creating it..#news.......I.....CassandraBot..Chat doesn't work right now, but it's a start...#news.............Connor..<3..LPOPYui....");
+//res.send("error: pass");
+//res.sendFile("C:\\Users\\lpopvm\\Desktop\\Bancho\\login.raw");
+});
+
+
+app.all('*', function(req, res) {
+/*
+res.set("Location","http://ranking.lpop.me:8021" + req.originalUrl);
+return res.end(res.writeHead(302, 'Redirect To Ranking'));
+        console.log("Handled Ranking Request: http://ranking.lpop.me:8021" + req.originalUrl);
+        */
+var temp=req.originalUrl.substring(1);
+if(temp.indexOf('.php') >= 0){
+  res.render(temp.split(".")[0]);
+} else {
+  res.sendFile("C:\\Users\\lpopme\\Downloads\\Server\\Server\\" + temp);
+}
+
+});
+
+var server = app.listen(80, function() {
+    console.log('Listening on port %d', server.address().port);
+});
