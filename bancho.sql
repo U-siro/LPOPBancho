b5043f315cd05968ef1f34f7e729552befbef58d
KangYui <castarnet@gmail.com>
2016-01-12 04:24:16 +0900

    Add Score Table

--- bancho.sql
+++ bancho.sql
@@ -1,58 +1,57 @@
--- phpMyAdmin SQL Dump
--- version 4.5.1
--- http://www.phpmyadmin.net
---
--- Host: 127.0.0.1
--- Generation Time: Jan 11, 2016 at 08:14 PM
--- Server version: 10.1.8-MariaDB
--- PHP Version: 5.6.14
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `bancho`
---
-
+-- --------------------------------------------------------
+-- 호스트:                          127.0.0.1
+-- 서버 버전:                        10.1.10-MariaDB - mariadb.org binary distribution
+-- 서버 OS:                        Win64
+-- HeidiSQL 버전:                  9.3.0.4984
 -- --------------------------------------------------------
 
---
--- Table structure for table `users`
---
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
-CREATE TABLE `users` (
-  `userID` int(11) NOT NULL,
-  `username` varchar(16) NOT NULL,
-  `passwordHash` varchar(50) NOT NULL,
-  `isBanned` int(11) NOT NULL,
-  `isAdmin` int(11) NOT NULL
+-- bancho 데이터베이스 구조 내보내기
+CREATE DATABASE IF NOT EXISTS `bancho` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `bancho`;
+
+
+-- 테이블 bancho.scores 구조 내보내기
+CREATE TABLE IF NOT EXISTS `scores` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `beatmapHash` varchar(32) DEFAULT NULL,
+  `playerID` varchar(5) DEFAULT NULL,
+  `count300` int(11) DEFAULT NULL,
+  `count100` int(11) DEFAULT NULL,
+  `count50` int(11) DEFAULT NULL,
+  `countKatu` int(11) DEFAULT NULL,
+  `countGeki` int(11) DEFAULT NULL,
+  `countMiss` int(11) DEFAULT NULL,
+  `score` int(11) DEFAULT NULL,
+  `combo` int(11) DEFAULT NULL,
+  `fc` tinyint(1) DEFAULT NULL,
+  `mods` int(11) DEFAULT NULL,
+  `time` varchar(18) DEFAULT NULL,
+  `mode` tinyint(4) DEFAULT NULL,
+  `completed` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `ID` (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Indexes for dumped tables
---
+-- 내보낼 데이터가 선택되어 있지 않습니다.
 
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`userID`);
 
---
--- AUTO_INCREMENT for dumped tables
---
+-- 테이블 bancho.users 구조 내보내기
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL,
+  `passwordHash` varchar(50) NOT NULL,
+  `isBanned` int(1) DEFAULT NULL,
+  `isAdmin` int(1) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT;
+-- 내보낼 데이터가 선택되어 있지 않습니다.
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
